c3b013e20e3faf85148c3bbdc6f44596bb69ff2f
Phong D. Nguyen <61719943+phongdnguyen@users.noreply.github.com>
2022-03-09 08:47:38 -0700

    Project Initialization
    This is a project I have been working on. I stripped down some features
    to meet the assignment requisite

---
+++ app/app.R
@@ -0,0 +1,240 @@
+library(shiny)
+library(shinyWidgets)
+library(ggplot2)
+library(tidync)
+library(dplyr)
+library(googledrive)
+
+# Bypass authentication for publicly available files
+drive_deauth()
+drive_user()
+
+# This operation will take some time to download the requisite data set.
+if (!file.exists("data")) {
+  dir.create("data")
+  drive_download("https://drive.google.com/uc?export=download&id=1s0LbIHZdnsOOKXpZKNccJXKQV9yySLzS", path = "./data/MAXT.nc")
+  drive_download("https://drive.google.com/uc?export=download&id=1L2d2Rvaos1FnYjLVK1nkF9lft1sSxxI7", path = "./data/MINT.nc")
+  drive_download("https://drive.google.com/uc?export=download&id=1OWR7_5kjKMc60dEFR6xC-5459pW6eflE", path = "./data/PREC.nc")
+}
+
+var.maxt <- tidync("data/MAXT.nc")
+var.mint <- tidync("data/MINT.nc")
+var.prec <- tidync("data/PREC.nc")
+
+source("map-data.R")
+
+#######################################################################################################
+#######################################         SERVER         ########################################
+#######################################################################################################
+server <- function(input, output, session) {
+  update <- list(
+    timeFilter = reactive({ input$timeFilter }),
+    coor = reactive({list(lon = input$lon, lat = input$lat)}),
+    basedata = reactive({
+      switch (
+        input$baseData,
+        "ERA-Interim" = {
+          maxt.slc <-
+            var.maxt %>% hyper_filter(time = between(
+              as.Date(time, origin = "1979-01-01"),
+              input$timeFilter,
+              input$timeFilter
+            )) %>% hyper_tibble()
+          mint.slc <-
+            var.mint %>% hyper_filter(time = between(
+              as.Date(time, origin = "1979-01-01"),
+              input$timeFilter,
+              input$timeFilter
+            )) %>% hyper_tibble()
+          prec.slc <-
+            var.prec %>% hyper_filter(time = between(
+              as.Date(time, origin = "1979-01-01"),
+              input$timeFilter,
+              input$timeFilter
+            )) %>% hyper_tibble()
+          
+          full_join(maxt.slc, mint.slc, by = c("lon", "lat", "time")) %>% full_join(prec.slc, by = c("lon", "lat", "time")) %>%
+            mutate(DIFF = MAXT - MINT, lon = lon - 360) %>%
+            select(lon, lat, MAXT, MINT, DIFF, PREC)
+        },
+      )
+    }),
+    linedata = reactive({
+        time <- input$timeFilter
+        multiplier <- input$trend
+        range <- c(time - (360 * multiplier), time + (360 * multiplier))
+        
+        maxt.slc <-
+          var.maxt %>% hyper_filter(
+            lon = lon == input$lon + 360,
+            lat = lat == input$lat,
+            time = between(as.Date(time, origin =
+                                     "1979-01-01"), range[1], range[2])
+          ) %>% hyper_tibble()
+        
+        mint.slc <-
+          var.mint %>% hyper_filter(
+            lon = lon == input$lon + 360,
+            lat = lat == input$lat,
+            time = between(as.Date(time, origin =
+                                     "1979-01-01"), range[1], range[2])
+          ) %>% hyper_tibble()
+        
+        prec.slc <-
+          var.prec %>% hyper_filter(
+            lon = lon == input$lon + 360,
+            lat = lat == input$lat,
+            time = between(as.Date(time, origin =
+                                     "1979-01-01"), range[1], range[2])
+          ) %>% hyper_tibble()
+        
+        full_join(maxt.slc, mint.slc, by = c("lon", "lat", "time")) %>% full_join(prec.slc, by = c("lon", "lat", "time")) %>%
+          mutate(DIFF = MAXT - MINT, lon = lon - 360) %>%
+          select(time, lon, lat, MAXT, MINT, DIFF, PREC)
+      })
+  )
+  
+  output$title <- renderText(input$baseData)
+  output$coor <- renderText(paste("lon:", input$plot_hover$x[1],
+                                  "lat:", input$plot_hover$y[1]))
+  
+  observeEvent(input$plot_click, {
+    x <- round((input$plot_click$x[1] + 136.5) / 0.75) * 0.75 - 136.5
+    y <- round((input$plot_click$y[1] - 17.25) / 0.75) * 0.75 + 17.25
+    
+    updateSliderInput(session, "lon", value = x)
+    updateSliderInput(session, "lat", value = y)
+  })
+  
+  #### MAIN TAB
+  output$maxt <- renderPlot({
+    base <- update$basedata()
+    
+    ggplot(base) +
+      ggtitle("Maximum") +
+      geom_raster(aes(x = lon, y = lat, fill = MAXT), interpolate = TRUE) +
+      scale_fill_distiller(palette = "Reds") +
+      base.map +
+      labs(x = NULL, y = NULL) +
+      geom_point(
+        x = update$coor()$lon,
+        y = update$coor()$lat,
+        color = "white",
+        shape = "cross",
+        size = 2
+      ) +
+      theme_bw()
+  })
+  output$mint <- renderPlot({
+    base <- update$basedata()
+    
+    ggplot(base) +
+      ggtitle("Minimum") +
+      geom_raster(aes(x = lon , y = lat, fill = MINT), interpolate = TRUE) +
+      base.map +
+      labs(x = NULL, y = NULL) +
+      geom_point(
+        x = update$coor()$lon,
+        y = update$coor()$lat,
+        color = "white",
+        shape = "cross",
+        size = 2
+      ) +
+      theme_bw()
+  })
+  output$diff <- renderPlot({
+    base <- update$basedata()
+    
+    ggplot(base) +
+      ggtitle("Difference") +
+      geom_raster(aes(x = lon, y = lat, fill = DIFF), interpolate = TRUE) +
+      scale_fill_distiller(palette = "Purples") +
+      base.map +
+      labs(x = NULL, y = NULL) +
+      geom_point(
+        x = update$coor()$lon,
+        y = update$coor()$lat,
+        color = "white",
+        shape = "cross",
+        size = 2
+      ) +
+      theme_bw()
+  })
+  output$prec <- renderPlot({
+    base <- update$basedata()
+    
+    ggplot(base) +
+      ggtitle("Precipitation") +
+      geom_raster(aes(x = lon, y = lat, fill = PREC), interpolate = TRUE) +
+      scale_fill_distiller(palette = "Greens") +
+      base.map +
+      labs(x = NULL, y = NULL) +
+      geom_point(
+        x = update$coor()$lon,
+        y = update$coor()$lat,
+        color = "white",
+        shape = "cross",
+        size = 2
+      ) +
+      theme_bw()
+  })
+  
+  output$line1 <- renderPlot({
+    base <- update$linedata()
+    
+    ggplot(base) +
+      geom_line(aes(x = as.Date(time, origin = "1979-01-01"), y = MAXT), color = "red") +
+      scale_x_date(date_breaks = "1 year", date_minor_breaks = "1 month") +
+      geom_vline(xintercept = update$timeFilter(),
+                 color = "orange") +
+      labs(x = NULL, y = NULL) +
+      theme_minimal() +
+      theme(axis.text.x = element_text(angle = 60, vjust = .5))
+  })
+  output$line2 <- renderPlot({
+    base <- update$linedata()
+    
+    ggplot(base) +
+      geom_line(aes(x = as.Date(time, origin = "1979-01-01"), y = MINT), color = "blue") +
+      scale_x_date(date_breaks = "1 year", date_minor_breaks = "1 month") +
+      geom_vline(xintercept = update$timeFilter(),
+                 color = "orange") +
+      labs(x = NULL, y = NULL) +
+      theme_minimal() +
+      theme(axis.text.x = element_text(angle = 60, vjust = .5))
+  })
+  output$line3 <- renderPlot({
+    base <- update$linedata()
+    
+    ggplot(base) +
+      geom_line(aes(
+        x = as.Date(time, origin = "1979-01-01"),
+        y = MAXT - MINT
+      ), color = "purple") +
+      scale_x_date(date_breaks = "1 year", date_minor_breaks = "1 month") +
+      geom_vline(xintercept = update$timeFilter(),
+                 color = "orange") +
+      labs(x = NULL, y = NULL) +
+      theme_minimal() +
+      theme(axis.text.x = element_text(angle = 60, vjust = .5))
+  })
+  output$line4 <- renderPlot({
+    base <- update$linedata()
+    
+    ggplot(base) +
+      geom_line(aes(x = as.Date(time, origin = "1979-01-01"), y = PREC), color = "green") +
+      scale_x_date(date_breaks = "1 year", date_minor_breaks = "1 month") +
+      geom_vline(xintercept = update$timeFilter(),
+                 color = "orange") +
+      labs(x = NULL, y = NULL) +
+      theme_minimal() +
+      theme(axis.text.x = element_text(angle = 60, vjust = .5))
+  })
+  
+  #### DATA TAB
+  output$basedata <- renderDataTable({
+    update$basedata()
+  })
+}
+
+shinyApp(htmlTemplate("../app/template.html"), server, enableBookmarking = "url")
---
+++ app/map-data.R
@@ -0,0 +1,34 @@
+library(maps)
+library(sf)
+library(tools)
+
+sf_use_s2(FALSE)
+
+#######################################################################################################
+####################################         BASE MAP DATA         #################################### 
+#######################################################################################################
+# This map data is from ggplot2. Map data can easily be replaced with raster maps from Google Maps, 
+# Statem, or OpenStreetMaps using the ggmaps package. The maps can be skinned with roads, terrain,
+# buildings, etc. This may be useful depending on what our research question will be.
+map <- st_as_sf(map("world", plot = FALSE, fill = TRUE))
+map <- cbind(map, st_coordinates(st_centroid(map)))
+map$ID <- toTitleCase(as.vector(map$ID))
+map$ID <- ifelse(map$ID == "USA", "", map$ID)
+
+us.states <- map_data("state")
+us.states <- st_as_sf(map("state", plot = FALSE, fill = TRUE))
+us.states <- cbind(us.states, st_coordinates(st_centroid(us.states)))
+us.states$ID <- toTitleCase(as.vector(us.states$ID))
+
+world.map <- st_as_sf(map("world", plot = FALSE, fill = TRUE))
+world.map <- cbind(map, st_coordinates(st_centroid(map)))
+
+base.map <- c(geom_sf(data = world.map, fill = NA),
+              geom_text(data = map, aes(X, Y, label = ID), size = 2),
+              geom_sf(data = us.states, fill = NA),
+              geom_text(data = us.states, aes(X, Y, label = ID), size = 2),
+              coord_sf(xlim = c(min(var.maxt$transforms$lon$lon) - 360,
+                                max(var.maxt$transforms$lon$lon) - 360),
+                       ylim = c(min(var.maxt$transforms$lat$lat),
+                                max(var.maxt$transforms$lat$lat)),
+                       expand = FALSE))
